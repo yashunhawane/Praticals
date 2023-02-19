5c79a23a7c55e06c0aa4072666fd9842c00d694c
Yash UNHAWANE <74054179+yashunhawane@users.noreply.github.com>
2023-02-19 22:06:57 +0530

    Add files via upload

---
+++ WEB/Angular/MainQuestion/First100Primes.js
@@ -0,0 +1,15 @@
+//create a program to print prime number till 100?
+function getPrimes(max) {
+    var sieve = [], i, j, primes = [];
+    for (i = 2; i <= max; ++i) {
+        if (!sieve[i]) {
+            // i has not been marked -- it is prime
+            primes.push(i);
+            for (j = i << 1; j <= max; j += i) {
+                sieve[j] = true;
+            }
+        }
+    }
+    return primes;
+}
+console.log(getPrimes(100));
